jjpprrrr/device_xiaomi_sdm845-common

sdm845-common: libperfmgr: cache display lpm state to avoid set it again
If current foss state same as desired state, doesn't set it again.

Bug: 148656663
Test: stress on/off battery saver and check foss state
Test: enable battery saver, stress on/off screen
Change-Id: I6298361fd6a5d6a86c94eabb71ab254507136fd3
Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

power-libperfmgr/disp-power/InteractionHandler.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "android.hardware.power@1.3-service.xiaomi-libperfmgr"
+#define LOG_TAG "android.hardware.power@-service.xiaomi-libperfmgr"
 #define ATRACE_TAG (ATRACE_TAG_POWER | ATRACE_TAG_HAL)
 
 #include <fcntl.h>

ugh5jhr/android_device_lge_sdm845-common

power-libperfmgr/disp-power/InteractionHandler.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "android.hardware.power@1.3-service.lge-libperfmgr"
+#define LOG_TAG "android.hardware.power@-service.lge-libperfmgr"
 #define ATRACE_TAG (ATRACE_TAG_POWER | ATRACE_TAG_HAL)
 
 #include <fcntl.h>