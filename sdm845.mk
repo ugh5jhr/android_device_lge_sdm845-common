e69cd122a62feb51986fd761b0f37fa655ee0292
Emanuel <emanuelghub@gmail.com>
2021-11-30 20:34:00 +0400

    sdm845-common: Use lg's zram implementation

--- sdm845.mk
+++ sdm845.mk
@@ -308,6 +308,7 @@ PRODUCT_PACKAGES += \
 # Init
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/rootdir/etc/ueventd.rc:$(TARGET_COPY_OUT_VENDOR)/ueventd.rc
+    $(COMMON_PATH)/rootdir/init.lge.svelte.rc:$(TARGET_COPY_OUT_VENDOR)/init.lge.svelte.rc
 
 $(foreach f,$(wildcard $(COMMON_PATH)/rootdir/etc/init/hw/*.rc),\
         $(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/$(notdir $f)))
