d3aa8c7454b7ea3068070e709b7e2adeb0f7d66a
ugh5jhr <fisbw92b7sv73@gmail.com>
2023-03-12 10:30:26 +0000

    rom not support kryo785

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -23,7 +23,7 @@ COMMON_PATH := device/lge/sdm845-common
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-2a
 TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_VARIANT := kryo785
+TARGET_CPU_VARIANT := kryo385
 
 ifeq (,$(filter %_64,$(TARGET_PRODUCT)))
 TARGET_2ND_ARCH := arm
